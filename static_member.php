5e480f5574d62eabb60a852c5e46a9b9781c405c
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-11 15:31:21 +0600

    static with out object

---
+++ static_member.php
@@ -0,0 +1,25 @@
+<?php
+
+class base
+{
+    public static $name = ' gulam kibria';
+    public static function show()
+    {
+        //echo $this->name; //this is not working bcz static variable is not accessible outside the class using self
+        echo self::$name;
+    }
+
+    // public function __construct($n)
+    // {
+    //     //echo $this->name;
+    //     self::$name = $n;
+    // }
+}
+
+echo base::$name;
+base::show();
+
+
+// $obj = new base('amir hamza');
+
+// $obj->show();
