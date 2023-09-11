7af8839b3b9107bcfaf18c5798995c26baa3c81f
Gulam Kibria Chowdhury <43819400+GK-CPP@users.noreply.github.com>
2023-09-11 13:01:00 +0600

    access modifier

--- access_modifier.php
+++ access_modifier.php
@@ -1,5 +1,63 @@
 <?php
 
+class base_public
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    public function show()
+    {
+
+        echo $this->name;
+    }
+}
+
+$obj = new base_public('gulam kibria');
+
+$obj->name = 'hamza chowdhury';
+
+$obj->show();
+
+/*******************************************************/
+
+
+class base_protected
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    protected function show()
+    {
+
+        echo $this->name;
+    }
+}
+
+class child extends base_protected
+{
+    public function get()
+    {
+        echo $this->name . '<br>';
+    }
+}
+
+$obj = new base_protected('gulam kibria');
+
+//Cannot access protected property base
+//$obj->name = 'hamza chowdhury'; 
+//$obj->show();
+
+$obj2 = new child('hamza chowdhury');
+
+$obj2->get();
+
+/*******************************************************/
 
 class base_private
 {
@@ -20,7 +78,7 @@ class child1 extends base_private
 {
     public function get()
     {
-        // echo $this->name . '<br>'; //name not found
+        // echo $this->name . '<br>'; //name not 
     }
 }
 
